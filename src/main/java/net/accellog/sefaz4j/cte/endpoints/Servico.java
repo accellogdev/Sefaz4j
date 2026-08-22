4f294dd9559c9a460c052f48aa3fe2c0ccccc8da
ValterFC <54070199+valter-accellog@users.noreply.github.com>
2026-08-22 19:35:48 -0300

    feat(cte): adicionar Servico.CTE_RECEPCAO_SINC e cte-servicos.ini

---
+++ src/main/java/net/accellog/sefaz4j/cte/endpoints/Servico.java
@@ -0,0 +1,15 @@
+package net.accellog.sefaz4j.cte.endpoints;
+
+public enum Servico {
+    CTE_RECEPCAO_SINC("CTeRecepcaoSinc");
+
+    private final String chaveIni;
+
+    Servico(String chaveIni) {
+        this.chaveIni = chaveIni;
+    }
+
+    public String getChaveIni() {
+        return chaveIni;
+    }
+}
---
+++ src/test/java/net/accellog/sefaz4j/cte/endpoints/EndpointResolverCTeTest.java
@@ -0,0 +1,24 @@
+package net.accellog.sefaz4j.cte.endpoints;
+
+import net.accellog.sefaz4j.endpoints.Ambiente;
+import net.accellog.sefaz4j.endpoints.EndpointResolver;
+import net.accellog.sefaz4j.endpoints.UF;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class EndpointResolverCTeTest {
+
+    private static final String CTE_INI = "/endpoints/cte-servicos.ini";
+    private static final String PREFIXO = "CTE_";
+
+    @Test
+    public void todasAs27UFsResolvemRecepcaoSincNosDoisAmbientes() {
+        for (UF uf : UF.values()) {
+            for (Ambiente ambiente : Ambiente.values()) {
+                String url = EndpointResolver.resolver(CTE_INI, PREFIXO, uf, ambiente, Servico.CTE_RECEPCAO_SINC.getChaveIni());
+                assertTrue(uf + "/" + ambiente + " não resolveu uma URL de CTeRecepcaoSinc", url != null && url.startsWith("http"));
+            }
+        }
+    }
+}
