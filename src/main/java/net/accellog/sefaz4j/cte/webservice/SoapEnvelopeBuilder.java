db871f157fa2781c3258a068814f98a7330b756a
ValterFC <54070199+valter-accellog@users.noreply.github.com>
2026-08-22 19:40:02 -0300

    feat(cte): adicionar SoapEnvelopeBuilder.envelopeRecepcaoSinc

---
+++ src/main/java/net/accellog/sefaz4j/cte/webservice/SoapEnvelopeBuilder.java
@@ -0,0 +1,19 @@
+package net.accellog.sefaz4j.cte.webservice;
+
+public final class SoapEnvelopeBuilder {
+
+    private SoapEnvelopeBuilder() {
+    }
+
+    public static String envelopeRecepcaoSinc(String cteXmlAssinado) {
+        return "<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+            "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
+            "xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">" +
+            "<soap12:Body>" +
+            "<cteDadosMsg xmlns=\"http://www.portalfiscal.inf.br/cte/wsdl/CTeRecepcaoSinc\">" +
+            cteXmlAssinado +
+            "</cteDadosMsg>" +
+            "</soap12:Body>" +
+            "</soap12:Envelope>";
+    }
+}
---
+++ src/test/java/net/accellog/sefaz4j/cte/webservice/SoapEnvelopeBuilderTest.java
@@ -0,0 +1,19 @@
+package net.accellog.sefaz4j.cte.webservice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SoapEnvelopeBuilderTest {
+
+    @Test
+    public void envelopeRecepcaoSincEnvolveOCteAssinadoNoWrapperCorreto() {
+        String cteAssinado = "<CTe xmlns=\"http://www.portalfiscal.inf.br/cte\"><infCte Id=\"CTe123\"/></CTe>";
+
+        String envelope = SoapEnvelopeBuilder.envelopeRecepcaoSinc(cteAssinado);
+
+        assertTrue(envelope.contains("<soap12:Envelope"));
+        assertTrue(envelope.contains("http://www.portalfiscal.inf.br/cte/wsdl/CTeRecepcaoSinc"));
+        assertTrue(envelope.contains(cteAssinado));
+    }
+}
