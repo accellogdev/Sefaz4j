5994605dbdf3303cbc775262a30bdd3053c56266
ValterFC <54070199+valter-accellog@users.noreply.github.com>
2026-08-22 23:37:15 -0300

    feat(cte): adicionar envelopeConsultaSituacao/envelopeRecepcaoEvento

--- src/main/java/net/accellog/sefaz4j/cte/webservice/SoapEnvelopeBuilder.java
+++ src/main/java/net/accellog/sefaz4j/cte/webservice/SoapEnvelopeBuilder.java
@@ -16,4 +16,28 @@ public final class SoapEnvelopeBuilder {
             "</soap12:Body>" +
             "</soap12:Envelope>";
     }
+
+    public static String envelopeConsultaSituacao(String consSitCTeXml) {
+        return "<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+            "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
+            "xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">" +
+            "<soap12:Body>" +
+            "<cteDadosMsg xmlns=\"http://www.portalfiscal.inf.br/cte/wsdl/CTeConsultaV4\">" +
+            consSitCTeXml +
+            "</cteDadosMsg>" +
+            "</soap12:Body>" +
+            "</soap12:Envelope>";
+    }
+
+    public static String envelopeRecepcaoEvento(String eventoCTeXmlAssinado) {
+        return "<soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+            "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
+            "xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\">" +
+            "<soap12:Body>" +
+            "<cteDadosMsg xmlns=\"http://www.portalfiscal.inf.br/cte/wsdl/CTeRecepcaoEventoV4\">" +
+            eventoCTeXmlAssinado +
+            "</cteDadosMsg>" +
+            "</soap12:Body>" +
+            "</soap12:Envelope>";
+    }
 }
--- src/test/java/net/accellog/sefaz4j/cte/webservice/SoapEnvelopeBuilderTest.java
+++ src/test/java/net/accellog/sefaz4j/cte/webservice/SoapEnvelopeBuilderTest.java
@@ -2,6 +2,7 @@ package net.accellog.sefaz4j.cte.webservice;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SoapEnvelopeBuilderTest {
@@ -15,5 +16,28 @@ public class SoapEnvelopeBuilderTest {
         assertTrue(envelope.contains("<soap12:Envelope"));
         assertTrue(envelope.contains("http://www.portalfiscal.inf.br/cte/wsdl/CTeRecepcaoSincV4"));
         assertTrue(envelope.contains(cteAssinado));
+    }
+
+    @Test
+    public void envelopeConsultaSituacaoEnvolveOXmlDeConsultaNoWrapperCorreto() {
+        String consSitCTe = "<consSitCTe xmlns=\"http://www.portalfiscal.inf.br/cte\" versao=\"4.00\"><tpAmb>2</tpAmb><xServ>CONSULTAR</xServ><chCTe>123</chCTe></consSitCTe>";
+
+        String envelope = SoapEnvelopeBuilder.envelopeConsultaSituacao(consSitCTe);
+
+        assertTrue(envelope.contains("<soap12:Envelope"));
+        assertTrue(envelope.contains("http://www.portalfiscal.inf.br/cte/wsdl/CTeConsultaV4"));
+        assertTrue(envelope.contains(consSitCTe));
+    }
+
+    @Test
+    public void envelopeRecepcaoEventoEnvolveOEventoAssinadoNoWrapperCorretoSemLote() {
+        String eventoAssinado = "<eventoCTe xmlns=\"http://www.portalfiscal.inf.br/cte\" versao=\"4.00\"><infEvento Id=\"ID1\"/></eventoCTe>";
+
+        String envelope = SoapEnvelopeBuilder.envelopeRecepcaoEvento(eventoAssinado);
+
+        assertTrue(envelope.contains("<soap12:Envelope"));
+        assertTrue(envelope.contains("http://www.portalfiscal.inf.br/cte/wsdl/CTeRecepcaoEventoV4"));
+        assertTrue(envelope.contains(eventoAssinado));
+        assertFalse("CTe não usa envelope de lote (idLote) para eventos", envelope.contains("idLote"));
     }
 }
